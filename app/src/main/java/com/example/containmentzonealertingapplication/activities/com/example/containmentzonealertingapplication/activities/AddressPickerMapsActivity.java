4ce71c8c947ef1f952e9560d1b0b562c4d53a30e
Snekha <vijisrutii@gmail.com>
2022-11-23 00:24:37 +0530

    Java

---
+++ app/src/main/java/com/example/containmentzonealertingapplication/activities/com/example/containmentzonealertingapplication/activities/AddressPickerMapsActivity.java
@@ -0,0 +1,13 @@
+package com.example.containmentzonealertingapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddressPickerMapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
